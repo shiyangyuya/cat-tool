d26de025df47e3f2ad2872178b2a0dac18779e62
yangyu <yangyu@scmttec.com>
2024-10-23 17:50:35 +0800

    docs: icon

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   title: "cat-tool",
   base: "/cat-tool/",
   themeConfig: {
+    logo: "https://img.scmttec.com/thematic%20pic/30/qe/9de531729676346446.gif",
     socialLinks: [
       { icon: "github", link: "https://github.com/shiyangyuya/cat-tool" },
     ],
